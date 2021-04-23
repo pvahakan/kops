d16ba05ba07ac97d347bb1b63db997fdd7732ed2
pvahakan <pauli.vahakangas@gmail.com>
2021-04-23 15:12:50 +0300

    XML-parser luotu sanalistan tekoa varten

--- typing_tester/main.py
+++ typing_tester/main.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
+import csv
+import xml.etree.ElementTree as ET
+
+path = '../kotus-sanalista_v1/kotus-sanalista_v1.xml'
+
+def parse_xml(xmlfile):
+    tree = ET.parse(xmlfile)
+    root = tree.getroot()
+
+    words = []
+
+    for item in root:
+        for child in item:
+            if child.tag == 's':
+                words.append(child.text)
+
+    return words
+
+def write_to_file(f):
+    with open(f, 'w') as writer:
+        for word in parse_xml(path):
+            if not word.startswith('à'):
+                writer.write(word.strip('-') + '\n')
+
 if __name__ == '__main__':
-    print('hello world')
+    write_to_file('sanalista.txt')
