82f03b7ba1dc39c54aa4ef09fc1b975d2eb7d903
pvahakan <pauli.vahakangas@gmail.com>
2021-05-03 21:24:19 +0300

    Testataan ssh:ta

--- typing_tester/main.py
+++ typing_tester/main.py
@@ -79,5 +79,4 @@ if __name__ == '__main__':
     print(correct_letters('MOI', 'moi'))
     print(correct_letters('mOoi', 'moi'))
     print(correct_letters('iom', 'moi'))
-    print(correct_letters('12ll12', 'moi'))
     # start()
